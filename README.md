2a4692b54ecec28568e36457716fa79e781d9edb
Kevin <kevin.sang.yu@gmail.com>
2023-07-14 18:43:37 +1000

    For campuses that have multiple levels, there will be different timetables per each level.
    Visualization has been finalised, except for finetuning the wrap text
    width for the classes. For some reason it is very fickle to work with.
    
    Updated the Readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # timetable-vis
-Tool to visualize time tables in csv/table format.
+Kevin Yu v1
+
+Install required modules:
+1. pandas
+2. matplotlib
+3. openpyxl
+
+This python tool renders timetable images from a single .xlsx file. The Excel spreadsheet file must be in the following 
+format:
+
+1. Every row must be a class/event
+2. The spreadsheet must start at A1, meaning that the first row **must** be the column headings
+3. The Following headings must be present (Caps Sensitive!):
+   1. Curriculum Item (aka Subject Code)
+   2. Full Title (aka Subject Name)
+   3. Activity Name (aka class type, eg Lecture)
+   4. Start Date
+   5. Start Time
+   6. End Time
+   7. Building Id
+   8. Room Id
+   9. Staff Family Name
+   10. Staff Given Name
+4. The third last character of the Room Id **must** denote the level of the room, eg OC203 --> Level 2
+
+The resulting images will be outputted into separated folders, under the output folder.
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 from calendar import day_name
 import matplotlib.pyplot as plt
 from os import mkdir
-# import openpyxl
 
 
 # Time Table Visualizer
@@ -10,10 +9,9 @@ from os import mkdir
 
 
 class TimeTableVis(object):
-    def __init__(self, excel_path, show_empty_rooms):
+    def __init__(self, excel_path):
         self.excel_path = excel_path
         self.timetable = {}
-        self.show_empty_rooms = show_empty_rooms
 
         self.comprehend_excel_file()
 
@@ -24,6 +22,7 @@ class TimeTableVis(object):
             print("ERROR: Remove any active filters on the excel sheet and retry. \nPress enter to exit.")
             input()
             return
+        print("Comprehending excel sheet file")
         for row in file.iloc():
             date = row["Start Date"]
             dayoftheweek = day_name[date.weekday()]
@@ -36,11 +35,22 @@ class TimeTableVis(object):
                 self.timetable[row["Building Id"]][dayoftheweek][row["Room Id"]] = [row]
             else:
                 self.timetable[row["Building Id"]][dayoftheweek][row["Room Id"]].append(row)
-        if self.show_empty_rooms:
-            self.standardize_rooms()
+        self.standardize_rooms_and_levels()
 
-    def standardize_rooms(self):
-        for campus in self.timetable:
+    def standardize_rooms_and_levels(self):
+        """
+        This function will make sure that all campuses are also split apart by their levels, and
+        make sure every classroom in the campus is present in the timetable.
+
+        Make sure that the third last character (number or letter) denotes the level of the classrooms, such as:
+        OC206 --> O'Connell Street level 2, room 6
+        M104 --> Market Street level 1, room 4
+        NCG02 --> New Castle level G (ground), room 2
+        Additionally, if the class is online, keep the first 3 letters of the campus as ONL, otherwise it will split
+        the online timetable into different folders.
+        """
+        copytable = self.timetable.copy()
+        for campus in copytable:
             rooms = []
             for dayofweek in self.timetable[campus]:
                 for room in self.timetable[campus][dayofweek]:
@@ -49,8 +59,32 @@ class TimeTableVis(object):
                 for room in rooms:
                     if room not in self.timetable[campus][dayofweek].keys():
                         self.timetable[campus][dayofweek][room] = []
+            if campus[0:3] == 'ONL':  # don't bother splitting into levels if it's online
+                continue
+            levels = set()
+            for room in rooms:
+                levels.add(str(room)[-3])
+            if len(levels) > 1:
+                print(f"Multiple levels found on campus {campus}")
+                for level in levels:
+                    print(f"Adding classes for f'{campus}L{level}'")
+                    self.timetable[f'{campus}L{level}'] = {'Monday': {}, 'Tuesday': {}, 'Wednesday': {},
+                                                           'Thursday': {}, 'Friday': {}, 'Saturday': {},
+                                                           'Sunday': {}}
+                    for room in rooms:
+                        if str(room)[-3] == level:
+                            for dayofweek in self.timetable[f'{campus}L{level}']:
+                                self.timetable[f'{campus}L{level}'][dayofweek][room] = [cls for cls in
+                                                                                        self.timetable[campus][
+                                                                                            dayofweek][room] if
+                                                                                        cls['Room Id'][-3] == level]
+
+                del self.timetable[campus]
 
     def text_display_timetable(self):
+        """
+        Displays the timetable in text format just for testing
+        """
         for campus in self.timetable:
             print(campus)
             for dayofweek in self.timetable[campus]:
@@ -61,8 +95,12 @@ class TimeTableVis(object):
                         print("------", cls["Start Time"])
 
     def generate_images(self):
+        """
+        Actually does the generation of timetable images
+        """
         for campus in self.timetable:
             for dayofweek in self.timetable[campus]:
+                print(f"Plotting campus {campus} for {dayofweek}")
                 fig, ax = plt.subplots(figsize=(12, 7))
                 rooms = []
                 for room in self.timetable[campus][dayofweek]:
@@ -77,14 +115,16 @@ class TimeTableVis(object):
                 ax.set_yticklabels(rooms)
                 ax.set_xticks(range(9, 23))
                 ax.set_xlabel('Time')
+
+                # Plotting Classes
                 for roomindex in range(len(rooms)):
                     for cls in self.timetable[campus][dayofweek][rooms[roomindex]]:
-                        print(f"Plotting campus {campus} on {dayofweek} for {rooms[roomindex]}")
                         start = float(cls["Start Time"]) / 10000
                         end = float(cls["End Time"]) / 10000
 
-                        bar = plt.bar(x=start, height=0.8, width=end-start-0.05, bottom=roomindex+0.6, color='#d9d9d9', ec='black', zorder=3, align='edge')
-
+                        bar = plt.bar(x=start, height=0.8, width=end - start - 0.05, bottom=roomindex + 0.6,
+                                      color='#d9d9d9', ec='black', zorder=3, align='edge')
+                        # bar.set(bbox=dict(boxstyle='round', color='#d9d9d9'))
                         starttime = str(cls["Start Time"])[0:-4] + ":" + str(cls["Start Time"])[-4:-2]
                         endtime = str(cls["End Time"])[0:-4] + ":" + str(cls["End Time"])[-4:-2]
 
@@ -94,21 +134,27 @@ class TimeTableVis(object):
                         text += f"{cls['Staff Given Name']} {cls['Staff Family Name']}"
 
                         barheight = bar.patches[0].get_height()
-                        barwidth = bar.patches[0].get_width()
                         barx, bary = bar.patches[0].xy
-                        t = plt.text(barx + 0.05, bary+barheight/2, text, ha='left', va='center', fontsize=7, wrap=True)
-                        t.set(bbox=dict(boxstyle='round', color='#d9d9d9'))
+                        t = plt.text(barx + 0.05, bary + barheight / 2, text, ha='left', va='center', fontsize=7,
+                                     wrap=True)
+                        wrapsize = (end - start) * 400
+                        t._get_wrap_line_width = lambda: wrapsize  # change this counter to adjust wrapping size
 
+                # Name and save
                 plt.title(campus + " - " + dayofweek, y=1.07)
+                try:
+                    mkdir("output")
+                except FileExistsError as e:
+                    pass
                 try:
                     mkdir(f'output/{campus}')
                 except FileExistsError as e:
                     pass
-                plt.savefig(f'output/{campus}/{dayofweek}.png', dpi=200)
+                plt.savefig(f'output/{campus}/{dayofweek}.png', dpi=600)
                 fig.clf()
                 plt.close(fig)
 
 
 if __name__ == '__main__':
-    ttv = TimeTableVis(excel_path="KOI_Class_Report_8496.xlsx", show_empty_rooms=False)
+    ttv = TimeTableVis(excel_path="KOI_Class_Report_8496.xlsx")
     ttv.generate_images()
